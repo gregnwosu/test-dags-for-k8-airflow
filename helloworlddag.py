e513b63fe2850e86f2b0e233f7dbfb6330686af1
Gregory Nwosu <greg.nwosu@gmail.com>
2019-04-10 14:54:30 +0100

    added a k8 operator.

--- helloworlddag.py
+++ helloworlddag.py
@@ -3,6 +3,9 @@ import datetime as dt
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.operators import KubernetesOperator
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.contrib.kubernetes.secret import Secret
 
 
 def print_world():
@@ -17,7 +20,80 @@ default_args = {
 }
 
 
-with DAG('airflow_tutorial_v01',
+secret_file = Secret('volume', '/etc/sql_conn',
+                     'airflow-secrets', 'sql_alchemy_conn')
+secret_env = Secret('env', 'SQL_CONN', 'airflow-secrets', 'sql_alchemy_conn')
+volume_mount = VolumeMount('test-volume',
+                           mount_path='/root/mount_file',
+                           sub_path=None,
+                           read_only=True)
+
+volume_config = {
+    'persistentVolumeClaim':
+    {
+        'claimName': 'test-volume'
+    }
+}
+volume = Volume(name='test-volume', configs=volume_config)
+
+affinity = {
+    'nodeAffinity': {
+        'preferredDuringSchedulingIgnoredDuringExecution': [
+            {
+                "weight": 1,
+                "preference": {
+                    "matchExpressions": {
+                        "key": "disktype",
+                        "operator": "In",
+                        "values": ["ssd"]
+                    }
+                }
+            }
+        ]
+    },
+    "podAffinity": {
+        "requiredDuringSchedulingIgnoredDuringExecution": [
+            {
+                "labelSelector": {
+                    "matchExpressions": [
+                        {
+                            "key": "security",
+                            "operator": "In",
+                            "values": ["S1"]
+                        }
+                    ]
+                },
+                "topologyKey": "failure-domain.beta.kubernetes.io/zone"
+            }
+        ]
+    },
+    "podAntiAffinity": {
+        "requiredDuringSchedulingIgnoredDuringExecution": [
+            {
+                "labelSelector": {
+                    "matchExpressions": [
+                        {
+                            "key": "security",
+                            "operator": "In",
+                            "values": ["S2"]
+                        }
+                    ]
+                },
+                "topologyKey": "kubernetes.io/hostname"
+            }
+        ]
+    }
+}
+
+tolerations = [
+    {
+        'key': "key",
+        'operator': 'Equal',
+        'value': 'value'
+    }
+]
+
+with DAG('pretzel_test',
          default_args=default_args,
          schedule_interval='0 * * * *',
          ) as dag:
@@ -28,6 +104,22 @@ with DAG('airflow_tutorial_v01',
                          bash_command='sleep 5')
     print_world = PythonOperator(task_id='print_world',
                                  python_callable=print_world)
+
+    k = KubernetesPodOperator(namespace='default',
+                              image="ubuntu:16.04",
+                              cmds=["bash", "-cx"],
+                              arguments=["echo", "10"],
+                              labels={"foo": "bar"},
+                              # secrets=[secret_file, secret_env],
+                              volume=[volume],
+                              volume_mounts=[volume_mount],
+                              name="test",
+                              task_id="kubernetes_task",
+                              affinity=affinity,
+                              is_delete_operator_pod=True,
+                              hostnetwork=False,
+                              tolerations=tolerations
+                              )
 
 
 print_hello >> sleep >> print_world
